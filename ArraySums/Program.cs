03d37d909121c2e0863b80fea8b455825b106db0
Daniel <danielmordelfer@gmail.com>
2023-03-17 17:55:02 +0000

    sum up lines and columns

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -12,17 +12,34 @@ namespace ArraySums
             int vertical = Convert.ToInt32(Console.ReadLine());
 
             float[,] matrix = new float[horizontal, vertical];
-            //matrix[vertical] = new int[horizontal];
-            
+            float[] sums_line = new float[vertical];
+            float[] sums_column = new float[horizontal];
+
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
                     Console.Write("Give me a float number : ");
-                    matrix[i,j] = float.Parse(Console.ReadLine());
+                    matrix[i, j] = float.Parse(Console.ReadLine());
                 }
-                
+            }
 
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sums_line[i] = sums_line[i] + matrix[i, j];
+                }
+                Console.WriteLine("The sum up of line " + (i+1) + " = " + sums_line[i].ToString());
+            }
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    sums_column[j] = sums_column[j] + matrix[i, j];
+                }
+                Console.WriteLine("The sum up of column " + (j+1) + " = " + sums_column[j].ToString());
             }
         }
     }
