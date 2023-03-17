f3360d593b19333ab00d662ae3ab2e3d60521a36
Daniel <danielmordelfer@gmail.com>
2023-03-17 13:28:14 +0000

    removing character from a string

---
+++ ChangeString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "This is a string!";
+            Console.WriteLine(x);
+            string y = "i";
+            Console.WriteLine(y);
+            string[] z = new string[] { "i" };
+            
+            foreach ( var c in z)
+            {
+                x = x.Replace(c, string.Empty);
+            }
+            Console.WriteLine(x);
+
+
+        }
+    }
+}
