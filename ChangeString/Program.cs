b5b24946888621345fe82812f12f4903ad465de0
Daniel <danielmordelfer@gmail.com>
2025-03-07 14:01:20 +0000

    Program that changes chars to x in a string

---
+++ ChangeString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Type a phrase:");
+            string ask = Console.ReadLine();
+            Console.WriteLine("Type a character:");
+            char ch = char.Parse(Console.ReadLine());
+
+            
+            foreach (char c in ask)
+            {
+                char x = c == ch ? 'x' : c;
+                Console.WriteLine(x);
+            }
+
+
+            
+        }
+    }
+}
