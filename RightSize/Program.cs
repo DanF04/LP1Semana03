188713f349c6b907910220b5a03fb21f017a66f5
Daniel <danielmordelfer@gmail.com>
2025-03-07 13:32:16 +0000

    The program reads the user input in args but only prints if the string is bigger than 3

---
+++ RightSize/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string s in args)
+            { 
+                if (s.Length > 3) 
+                {
+                    Console.WriteLine(s);
+                }
+                
+            }
+        }
+    }
+}
