aaf041bb2a8c7b6b906edd110a131a39cd89bdfb
Daniel <danielmordelfer@gmail.com>
2025-03-07 15:14:14 +0000

    New program with perks

---
+++ HeroPerk/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace HeroPerk
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+
+             if (args.Length == 0)
+            {
+                Console.WriteLine("!No perks at all!");
+                Console.WriteLine("!Not gonna make it!");
+                return;
+            }
+
+            Perks perks = 0;
+
+            foreach (char s in args[0])
+            {
+                switch (s)
+                {
+                    case 'w':
+                        perks |= Perks.WarpShift;
+                        break;
+                    case 'a':
+                        perks |= Perks.Stealth;
+                        break;
+                    case 's':
+                        perks |= Perks.AutoHeal;
+                        break;
+                    case 'd':
+                        perks |= Perks.DoubleJump;
+                        break;
+                    default:
+                        Console.WriteLine("!Unknown perk!");
+                        return;
+                }
+            }
+            if (perks == 0)
+            {
+                Console.WriteLine("!No perks at all!");
+                Console.WriteLine("!Not gonna make it!");
+                return;
+            }
+            var activePerks = Enum.GetValues(typeof(Perks))
+                                      .Cast<Perks>()
+                                      .Where(p => perks.HasFlag(p))
+                                      .Select(p => p.ToString())
+                                      .ToArray();
+            Console.WriteLine(string.Join(", ", activePerks));
+
+            if (perks.HasFlag(Perks.Stealth) && perks.HasFlag(Perks.DoubleJump))
+            {
+                Console.WriteLine("!Silent jumper!");
+            }
+            if (!perks.HasFlag(Perks.AutoHeal))
+            {
+                Console.WriteLine("!Not gonna make it!");
+            }
+
+        }
+    }
+}
